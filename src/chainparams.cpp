54b36a00576b631e860b1cc95d10df8cea4290dd
TFinch <nexusnow@protonmail.com>
2022-11-28 22:07:28 -0500

    Fix incorrect genesis

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -457,7 +457,7 @@ public:
         tokenSubFee = 0 * COIN;
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
-        genesis = CreateGenesisBlock(1664023996, 3, 0x207fffff, 1, 0 * COIN);
+        genesis = CreateGenesisBlock(1665871306, 1, 0x207fffff, 1, 0 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
         consensus.nPosTargetSpacing = 6;
         consensus.height_last_PoW = 150;
@@ -468,7 +468,6 @@ public:
         pchMessageStart[0] = 0xff;
         vFixedSeeds.clear();
         vSeeds.clear();
-        consensus.nTimeSlotLength = 1;
         ////////////////////////////////////////////////////////////////////////////////////////////////////
     }
 
