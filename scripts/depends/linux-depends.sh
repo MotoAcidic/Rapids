d8b4b92e18164482a37750dba990e86dcd8934af
MotoAcidic <nexusnow@protonmail.com>
2022-12-01 17:02:17 -0500

    stage for home

--- scripts/depends/linux-depends.sh
+++ scripts/depends/linux-depends.sh
@@ -5,7 +5,7 @@ sudo apt install build-essential libtool autotools-dev automake pkg-config bsdma
 sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
 sudo apt update && apt install build-essential checkinstall zlib1g-dev libtemplate-perl -y
 sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo add-apt-repository ppa:pivx/pivx -y
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
