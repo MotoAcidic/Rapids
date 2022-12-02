8c41307b4f54fda445c5033e8d712ea190c580f4
TFinch <nexusnow@protonmail.com>
2022-12-01 21:18:59 -0500

    remove -y

--- scripts/depends/linux-depends.sh
+++ scripts/depends/linux-depends.sh
@@ -1,38 +1,25 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git python3 -y
-sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
-sudo apt update && apt install build-essential checkinstall zlib1g-dev libtemplate-perl -y
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:pivx/pivx -y
-sudo apt-get update -y
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
-sudo apt-get install libzmq3-dev -y
-sudo apt-get install libminiupnpc-dev -y
-sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev openssl -y
-sudo apt-get install libssl1.0-dev -y
-sudo apt-get install -y autoconf g++ make openssl libssl-dev libcurl4-openssl-dev -y
-sudo apt-get install -y libcurl4-openssl-dev pkg-config -y
-sudo apt-get install -y libsasl2-dev libgmp-dev -y
-sudo apt install libqt5svg5-dev -y
-sudo apt-get install -y libsodium-dev cargo -y
-cd
-
-wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-tar -xzvf db-4.8.30.NC.tar.gz
-cd db-4.8.30.NC/build_unix/
-../dist/configure --enable-cxx
-cd
-sed -i 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' db-4.8.30.NC/dbinc/atomic.h
-cd db-4.8.30.NC/build_unix/
-make
-make install
-cd
-
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git python3
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt update && apt install build-essential checkinstall zlib1g-dev libtemplate-perl
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:pivx/pivx
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+sudo apt-get install libzmq3-dev
+sudo apt-get install libminiupnpc-dev
+sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev openssl
+sudo apt-get install libssl1.0-dev
+sudo apt-get install autoconf g++ make openssl libssl-dev libcurl4-openssl-dev
+sudo apt-get install libcurl4-openssl-dev pkg-config
+sudo apt-get install libsasl2-dev libgmp-dev
+sudo apt install libqt5svg5-dev
+sudo apt-get install libsodium-dev cargo
 sudo apt-get install libssl-dev
 sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 libqt5svg5-dev libqt5charts5-dev qttools5-dev qttools5-dev-tools libqrencode-dev
 sudo apt install protobuf-compiler
-cd Rapids/scripts/build
+cd ../build
 bash compile-linux.sh
