9081b1232513c7cb0c0376ec2568de1d772a4cc6
TFinch <nexusnow@protonmail.com>
2022-12-02 20:48:21 -0500

    t

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -136,26 +136,19 @@ public:
         networkID = CBaseChainParams::MAIN;
         strNetworkID = "main";
         
-        if (genesis.GetHash() != uint256("0x")) {
-            printf("MSearching for genesis block...\n");
-            uint256 hashTarget;
-            hashTarget.SetCompact(genesis.nBits);
-            while (uint256(genesis.GetHash()) > uint256(hashTarget)) {
-                ++genesis.nNonce;
-                if (genesis.nNonce == 0) {
-                    printf("Mainnet NONCE WRAPPED, incrementing time");
-                    std::cout << std::string("Mainnet NONCE WRAPPED, incrementing time:\n");
-                    ++genesis.nTime;
-                }
-                if (genesis.nNonce % 1 == 0) {
-                    printf("Mainnet: nonce %08u: hash = %s \n", genesis.nNonce, genesis.GetHash().ToString().c_str());
-                }
+        genesis.nNonce = 99999;
+        printf("Searching genesis ...");
+        while (!DoubleCheckProofOfWork(genesis.GetHash(), genesis.nBits)) {
+            genesis.nNonce--;
+            if (genesis.nNonce == 0) {
+                printf("Nonce wrapped, shifting time");
+                genesis.nTime++;
+                genesis.nNonce = 99999;
             }
-            printf("Mainnet block.nTime = %u \n", genesis.nTime);
-            printf("Mainnet block.nNonce = %u \n", genesis.nNonce);
-            printf("Mainnet block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
-            printf("Mainnet block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
         }
+        printf("genesis.nTime = %u \n", genesis.nTime);
+        printf("genesis.nNonce = %u \n", genesis.nNonce);
+        printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
 
         //genesis = CreateGenesisBlock(1626521690, 4999564, 0x1e0ffff0, 1, 0 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
