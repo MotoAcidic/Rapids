d4f798b2095a9a57e82ac827cc20aa3e85fa0dbd
TFinch <nexusnow@protonmail.com>
2022-12-04 16:37:27 -0500

    Revert "test first file"
    This reverts commit 6341da2fda074de4bdb5670dc3c98fdd53846783.

--- src/main.cpp
+++ src/main.cpp
@@ -2274,7 +2274,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
         return true;
     }
 
-    const bool isPoSActive = nHeight >= consensus.height_last_PoW;
+    const bool isPoSActive = consensus.NetworkUpgradeActive(pindex->nHeight, Consensus::UPGRADE_POS);
     if (!isPoSActive && block.IsProofOfStake())
         return state.DoS(100, error("ConnectBlock() : PoS period not active"),
             REJECT_INVALID, "PoS-early");
