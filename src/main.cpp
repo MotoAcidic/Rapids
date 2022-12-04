9b2bbafb0aa5ff32f144c832be0ea11542b57e16
TFinch <nexusnow@protonmail.com>
2022-12-04 16:58:01 -0500

    a

--- src/main.cpp
+++ src/main.cpp
@@ -2274,12 +2274,11 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
         return true;
     }
 
-    const bool isPoSActive = chainActive.Height() >= consensus.height_last_PoW;
-    if (!isPoSActive && block.IsProofOfStake())
+    if (pindex->nHeight <= Params().LAST_POW_BLOCK() && block.IsProofOfStake())
         return state.DoS(100, error("ConnectBlock() : PoS period not active"),
             REJECT_INVALID, "PoS-early");
 
-    if (isPoSActive && block.IsProofOfWork())
+    if (pindex->nHeight > Params().LAST_POW_BLOCK() && block.IsProofOfWork())
         return state.DoS(100, error("ConnectBlock() : PoW period ended"),
             REJECT_INVALID, "PoW-ended");
 
