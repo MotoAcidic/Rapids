94a28e72774be5fd50c61558e332df044e94ce8a
TFinch <nexusnow@protonmail.com>
2022-12-04 08:36:11 -0500

    Revert "Correct brackets"
    This reverts commit e7bf9b4e0221fcb5bce0a202c12d77d292380455.

--- src/pow.cpp
+++ src/pow.cpp
@@ -154,14 +154,14 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     // Check proof of work matches claimed amount
-    if (UintToArith256(hash) > bnTarget) {
-        if (Params().MineBlocksOnDemand()) {
+    if (UintToArith256(hash) > bnTarget)
+        if (Params().MineBlocksOnDemand())
             return false;
-        } else {
+        else
             return error("CheckProofOfWork() : hash doesn't match nBits");
-        }
-        return true;
     }
+
+    return true;
 }
 
 arith_uint256 GetBlockProof(const CBlockIndex& block)
