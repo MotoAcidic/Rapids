62eadebf0972672ad2894a0d389c9a39ded4612a
TFinch <nexusnow@protonmail.com>
2022-12-04 21:00:13 -0500

    Rework spork19

--- src/miner.cpp
+++ src/miner.cpp
@@ -646,32 +646,14 @@ void RpdMiner(CWallet* pwallet, bool fProofOfStake)
                 utxo_dirty = false;
             }
 
-            if (sporkManager.IsSporkActive(SPORK_19_STAKE_SKIP_MN_SYNC)) {
-
-                    while ((g_connman && g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0 && Params().MiningRequiresPeers()) || pwallet->IsLocked() || !fStakeableCoins) {
-                        MilliSleep(5000);
-                        // Do a separate 1 minute check here to ensure fStakeableCoins is updated
-                        if (!fStakeableCoins) CheckForCoins(pwallet, 1, &availableCoins);
-                    }
-
-                
-            } else if (!sporkManager.IsSporkActive(SPORK_19_STAKE_SKIP_MN_SYNC)) {
-
-                    while ((g_connman && g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0 && Params().MiningRequiresPeers()) || pwallet->IsLocked() || !fStakeableCoins || !masternodeSync.NotCompleted()) {
-                        MilliSleep(5000);
-                        // Do a separate 1 minute check here to ensure fStakeableCoins is updated
-                        if (!fStakeableCoins) CheckForCoins(pwallet, 1, &availableCoins);
-
-                    }                
-            }
-
-
-                while ((g_connman && g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0 && Params().MiningRequiresPeers())
-                        || pwallet->IsLocked() || !fStakeableCoins || masternodeSync.NotCompleted()) {
+            if (!sporkManager.IsSporkActive(SPORK_19_STAKE_SKIP_MN_SYNC))
+            {
+                while ((g_connman && g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0 && Params().MiningRequiresPeers()) || pwallet->IsLocked() || !fStakeableCoins || masternodeSync.NotCompleted()) {
                     MilliSleep(5000);
                     // Do a separate 1 minute check here to ensure fStakeableCoins is updated
                     if (!fStakeableCoins) CheckForCoins(pwallet, 1, &availableCoins);
                 }
+            }
 
             //search our map of hashed blocks, see if bestblock has been hashed yet
             if (pwallet->pStakerStatus &&
