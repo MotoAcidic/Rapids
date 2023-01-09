59f1b25e4c6963e9020d1e6c33dbc6d001f217c8
MotoAcidic <nexusnow@protonmail.com>
2023-01-09 15:37:47 -0500

    test

--- src/pow.cpp
+++ src/pow.cpp
@@ -13,6 +13,7 @@
 #include "primitives/block.h"
 #include "uint256.h"
 #include "util.h"
+#include "spork.h"
 
 #include <math.h>
 
@@ -38,7 +39,8 @@ static arith_uint256 GetTargetLimit(int64_t nTime, bool fProofOfStake, const Con
 
 unsigned int Lwma3CalculateNextWorkRequired(const CBlockIndex* pindexLast, bool fProofOfStake, const Consensus::Params& params)
 {
-    const int64_t T = params.nPosTargetSpacing; // Default 6 seconds, defined by spork 
+    //const int64_t T = params.nPosTargetSpacing; // Default 6 seconds, defined by spork 
+    const int64_t T = sporkManager.GetSporkValue(SPORK_21_POS_TARGET_SPACING); // Default 6 seconds, defined by spork 
     const int64_t N = 8;
     const int64_t k = N * (N + 1) * T / 2; // 162
     const int64_t height = pindexLast->nHeight;
